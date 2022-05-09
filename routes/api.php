a03a31f813881d0118ec43e6007dd9f92674d127
SusanaChen03 <you@example.com>
2022-05-09 16:23:18 +0200

    create route for users

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Route::post('/user', function () {
+//     return 'CREATE USER BY ID';
+// });
+
+
+//localhost:8000/api/users
+
+
+Route::post('/user', [UserController::class, 'createNewUser']);  //created1
+
+Route::post('/register', [UserController::class, 'registerUser']);    //authoritation
+
+Route::post('/login', [UserController::class, 'loginUser']);    //authoritation
+
+Route::post('/logout', [UserController::class, 'logoutUser']);    //authoritation
+
+Route::get('/users', [UserController::class, 'getAllUsers']);    //for admin
+
+Route::get('/user/{id}', [UserController::class, 'getUserById']);
+
+Route::patch('/user/{id}', [UserController::class, 'updateUserById']);
+
+Route::delete('/user/{id}', [UserController::class, 'deleteUserById']);
