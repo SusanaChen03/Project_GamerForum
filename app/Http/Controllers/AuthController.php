69e4822017ae0c492c0f534bc5b72bff22076088
SusanaChen03 <you@example.com>
2022-05-10 22:42:15 +0200

    process install JWT, Route with middleware jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 
 class AuthController extends Controller
@@ -96,6 +97,21 @@ class AuthController extends Controller
                 'success' => false,
                 'message' => 'Sorry, the user cannot be logged out'
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getMyProfile()
+    {
+        try {
+            Log::info('Init Get Profile');
+
+            return response()->json(auth()->user());
+
+        } catch (\Throwable $th) {
+
+            Log::error('failed to get your profile->'.$th->getMessage());
+
+            return response()->json(['error=> error profile'],500);
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,33 +5,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 
 // Route::post('/user', function () {
 //     return 'CREATE USER BY ID';
 // });
 
 
-//localhost:8000/api/users
-
-
+//localhost:8000/api/
 Route::post('/user', [UserController::class, 'createNewUser']);  //created1
 Route::get('/user/{id}', [UserController::class, 'getUserById']);   //created1
 Route::patch('/user/{id}', [UserController::class, 'updateUserById']);    //created1
 Route::delete('/user/{id}', [UserController::class, 'deleteUserById']);   //created1
 
+Route::group([
+    'middleware'=> 'IsAdmin'
+], function(){
 Route::get('/users', [UserController::class, 'getAllUsers']);    //for admin
+});
 
 
+Route::group([
+    'middleware' => 'jwt.auth'
+], function(){
+Route::post('/register', [AuthController::class, 'registerUser']);    //authoritation
+Route::post('/login', [AuthController::class, 'loginUser']);    //authoritation
+Route::post('/logout', [AuthController::class, 'logoutUser']);    //authoritation
+Route::get('myProfile', [AuthController::class, 'getMyProfile']);  //authoritation
+});
 
-Route::post('/register', [UserController::class, 'registerUser']);    //authoritation
-
-Route::post('/login', [UserController::class, 'loginUser']);    //authoritation
-
-Route::post('/logout', [UserController::class, 'logoutUser']);    //authoritation
 
 
 
