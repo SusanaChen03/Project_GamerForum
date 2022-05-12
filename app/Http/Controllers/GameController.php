f332215b8a43b59d04110529a0f54831945121f8
SusanaChen03 <you@example.com>
2022-05-12 15:47:05 +0200

    sintaxis error

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
