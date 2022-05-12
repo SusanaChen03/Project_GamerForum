a848a47cc0d0d522d23c6189b54b4f38973eb50b
SusanaChen03 <you@example.com>
2022-05-12 14:03:37 +0200

    step1: create a new table in migrations (channels)

---
+++ database/migrations/2022_05_12_115219_create_channels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->unsignedBigInteger('game_id');
+            $table->foreign('game_id')->references('id')->on('game')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+};
